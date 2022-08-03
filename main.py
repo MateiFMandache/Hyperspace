b2edb9df33f95f724ab585c1ecfc0b2a0f12ef03
Matei Mandache <matei.mandache@gmail.com>
2022-08-03 17:16:29 +0100

    Shape choice: Cross added

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import math
 import tkinter as tk
 import numpy as np
-from itertools import product
+import enum
+from itertools import product, combinations
+from random import random
 
 
 VIEW_HEIGHT = 600
 VIEW_WIDTH = 800
 VIEW_CENTRE_X = VIEW_WIDTH // 2
 VIEW_CENTRE_Y = VIEW_HEIGHT // 2
-UNIT_LENGTH = 70
+UNIT_LENGTH = 150
 START_DIMENSION = 3
 MAX_DIMENSION = 7
 MIN_DIMENSION = 2
@@ -19,6 +21,13 @@ def degrees_of_freedom(dimension):
 
 
 MAX_DOF = degrees_of_freedom(MAX_DIMENSION)
+RANDOM_OFFSETS = [random() for _ in range(MAX_DOF)]
+
+
+class Form(enum.Enum):
+    CUBE = "Cube"
+    CROSS = "Cross"
+    SIMPLEX = "Simplex"
 
 
 def to_screen(pair):
@@ -59,6 +68,15 @@ class Shape:
             x2, y2 = to_screen(np.matmul(mat, v2)[:2])
             surface.create_line(x1, y1, x2, y2)
 
+    @classmethod
+    def make(cls, dimension, form):
+        if form == Form.CUBE:
+            return cls(dimension, cube_edges(dimension))
+        if form == Form.CROSS:
+            return cls(dimension, cross_edges(dimension))
+        if form == Form.SIMPLEX:
+            return cls(dimension, simplex_edges(dimension))
+
 
 def cube_edges(dimension):
     edges = []
@@ -69,16 +87,31 @@ def cube_edges(dimension):
     return edges
 
 
+def cross_edges(dimension):
+    pre_vertices = np.identity(dimension)
+    vertices_plus = [pre_vertices[i] for i in range(dimension)]
+    vertices_minus = [-pre_vertices[i] for i in range(dimension)]
+    vertices = vertices_plus + vertices_minus
+    edges = filter(lambda edge: any(-edge[0] != edge[1]), combinations(vertices, 2))
+    return list(edges)
+
+
+def simplex_edges(dimension):
+    vertices = [np.identity(dimension)[i] for i in range(dimension)]
+    return list(combinations(vertices, 2))
+
+
 def main():
     dimension = START_DIMENSION
+    form = Form.CUBE
+    shape = Shape.make(dimension, form)
 
     win = tk.Tk()
     view = tk.Canvas(win, width=VIEW_WIDTH, height=VIEW_HEIGHT)
     view.grid(row=1, column=0, columnspan=2)
 
     def update_shape(_):
-        shape = Shape(dimension, cube_edges(dimension))
-        shape.draw(view, [value.get() for value in values])
+        shape.draw(view, [value.get() + offset for value, offset in zip(values, RANDOM_OFFSETS)])
 
     slider_frame = tk.Frame(win)
     slider_frame.grid(row=0, column=1)
@@ -98,14 +131,23 @@ def main():
     settings_frame.grid(row=0, column=0)
     dimensions_label = tk.Label(settings_frame, text=f"{dimension} dimensions")
     dimensions_label.grid(row=0, column=1)
+    shape_choice = tk.Frame(settings_frame)
+    shape_choice.grid(row=1, column=0, columnspan=2)
 
     def change_dimension(new_dimension):
         if MIN_DIMENSION <= new_dimension <= MAX_DIMENSION:
-            nonlocal dimension
+            nonlocal dimension, shape
             dimension = new_dimension
             dimensions_label['text'] = f"{dimension} dimensions"
             show_sliders()
+            shape = Shape.make(dimension, form)
             update_shape(None)
+
+    def change_form(new_form):
+        nonlocal form, shape
+        form = new_form
+        shape = Shape.make(dimension, form)
+        update_shape(None)
 
     dimension_minus_button = tk.Button(settings_frame, text="-",
                                        command=lambda: change_dimension(dimension - 1))
@@ -113,6 +155,14 @@ def main():
     dimension_plus_button = tk.Button(settings_frame, text="+",
                                       command=lambda: change_dimension(dimension + 1))
     dimension_plus_button.grid(row=0, column=2, sticky=tk.W)
+
+    form_var = tk.StringVar(shape_choice, "Cube")
+    tk.Radiobutton(shape_choice, text="Cube", variable=form_var, value="Cube",
+                   command=lambda: change_form(Form.CUBE)).pack()
+    tk.Radiobutton(shape_choice, text="Cross", variable=form_var, value="Cross",
+                   command=lambda: change_form(Form.CROSS)).pack()
+    tk.Radiobutton(shape_choice, text="Simplex", variable=form_var, value="Simplex",
+                   command=lambda: change_form(Form.SIMPLEX)).pack()
 
     update_shape(None)
     win.mainloop()
